c953e2b7d8d424764e8110fac6aa038a37a3ef33
Devjit Choudhury <devjitc1@gmail.com>
2022-07-24 17:27:04 +0530

    feat: created tabsbar component

---
+++ src/components/TabsBar/TabsBar.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="tabsBar" class="noSelect pointerCursor">
+        <button id="createNewCircuitScope" class="logixButton" onclick="">
+            &#43;
+        </button>
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
