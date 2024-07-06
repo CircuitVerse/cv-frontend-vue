1153177f72f6ee7c680f8b53813f4152e42373a3
Aryann Dwivedi <aryanndwivedi2403@gmail.com>
2024-07-06 18:35:25 +0530

    fix: refactor script to read versions dynamically from versions.json instead of using a hardcoded array

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
 
-versions=("v0" "v1" )
+
+versions=($(jq -r '.[].version' version.json))
+
 
 for version in "${versions[@]}"; do
   echo "Building for version: $version"
-  npx vite build --config vite.config.$version.ts
+  
+  npx vite build --config vite.config."$version".ts
+  
+  #Build status
   if [ $? -ne 0 ]; then
     echo "Build failed for version: $version"
     exit 1
