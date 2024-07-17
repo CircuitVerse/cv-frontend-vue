bfb8526022d530937c289cd4adcd60417e2aec9f
Niladri Adhikary <91966855+niladrix719@users.noreply.github.com>
2024-07-17 18:09:46 +0530

    feat(gsoc'24): Typescript Integration combinationalAnalysis file

---
+++ src/simulator/src/combinationalAnalysis.ts
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-cycle */
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+import { scheduleBackup } from './data/backupCircuit'
+import { SimulatorStore } from '#/store/SimulatorStore/SimulatorStore'
+
+// var inputSample = 5
+// var dataSample = [
+//     ['01---', '11110', '01---', '00000'],
+//     ['01110', '1-1-1', '----0'],
+//     ['01---', '11110', '01110', '1-1-1', '0---0'],
+//     ['----1'],
+// ]
+
+// var sampleInputListNames = ['A', 'B']
+// var sampleOutputListNames = ['X']
+
+/**
+ * The prompt for combinational analysis
+ */
+export function createCombinationalAnalysisPrompt(scope = globalScope) {
+    scheduleBackup()
+    SimulatorStore().dialogBox.combinationalanalysis_dialog = true
+}
