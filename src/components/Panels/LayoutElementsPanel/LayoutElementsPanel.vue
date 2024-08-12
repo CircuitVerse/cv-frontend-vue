203b0e99df5d72596e1bf5fd1bb165679216af3e
Niladri Adhikary <rickyrickado14@gmail.com>
2024-08-12 20:40:02 +0530

    dropdown added for layoutmode

--- src/components/Panels/LayoutElementsPanel/LayoutElementsPanel.vue
+++ src/components/Panels/LayoutElementsPanel/LayoutElementsPanel.vue
@@ -1,36 +1,34 @@
 <template>
-  <div class="noSelect defaultCursor layoutElementPanel draggable-panel draggable-panel-css" ref="layoutElementPanelRef">
+  <div class="noSelect defaultCursor layoutElementPanel draggable-panel draggable-panel-css"
+    ref="layoutElementPanelRef">
     <div class="panel-header">
       {{ $t('simulator.layout.layout_elements') }}
       <span class="fas fa-minus-square minimize"></span>
       <span class="fas fa-external-link-square-alt maximize"></span>
     </div>
     <div class="panel-body">
-      <div class="search-results"></div>
-      <div id="subcircuitMenu" class="accordion">
-        <div v-for="(group, groupIndex) in SimulatorState.subCircuitElementList" :key="groupIndex">
-          <div class="panelHeader">{{ group.type }}s</div>
-          <div class="panel">
-            <div
-              v-for="(element, elementIndex) in group.elements"
-              class="icon subcircuitModule"
-              :key="`${groupIndex}-${elementIndex}`"
-              :id="`${group.type}-${elementIndex}`" :data-element-id="elementIndex" :data-element-name="group.type"
-              @mousedown="dragElement(group.type, element, elementIndex)"
-            >
+      <v-expansion-panels id="menu" class="accordion" variant="accordion">
+        <v-expansion-panel v-for="(group, groupIndex) in SimulatorState.subCircuitElementList" :key="groupIndex">
+          <v-expansion-panel-title>
+            {{ group.type }}s
+          </v-expansion-panel-title>
+          <v-expansion-panel-text eager>
+            <div class="panel customScroll">
+              <div v-for="(element, elementIndex) in group.elements" class="icon subcircuitModule"
+              :key="`${groupIndex}-${elementIndex}`" :id="`${group.type}-${elementIndex}`"
+              :data-element-id="elementIndex" :data-element-name="group.type"
+              @mousedown="dragElement(group.type, element, elementIndex)">
               <div class="icon-image">
                 <img :src="getImgUrl(group.type)" />
                 <p class="img__description">
                   {{ element.label !== '' ? element.label : $t('simulator.unlabeled') }}
                 </p>
               </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div v-if="SimulatorState.subCircuitElementList.length === 0">
-          <p>{{ $t('simulator.layout.no_elements_available') }}</p>
-        </div>
-      </div>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </div>
   </div>
 </template>
