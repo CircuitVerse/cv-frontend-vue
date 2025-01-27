855e109fe7f80ae0c362364a893812944a428932
Harsh Rao <harshraocodesup@gmail.com>
2025-01-28 04:49:20 +0530

    refactor for ts integration

---
+++ src/simulator/src/verilogHelpers.ts
@@ -0,0 +1,50 @@
+interface Node {
+    verilogLabel?: string;
+    parent: {
+        verilogLabel: string;
+    };
+    label?: string;
+}
+
+export function sanitizeLabel(name: string): string {
+    let temp = name;
+    // if there is a space anywhere but the last place
+    // replace spaces by "_"
+    // last space is required for escaped id
+    if (temp.search(/ /g) < temp.length - 1 && temp.search(/ /g) >= 0) {
+        temp = temp.replace(/ Inverse/g, '_inv');
+        temp = temp.replace(/ /g, '_');
+    }
+    // if first character is not \ already
+    if (temp.substring(0, 1).search(/\\/g) < 0) {
+        // if there are non-alphanum_ character, or first character is num, add \
+        if (
+            temp.search(/[\W]/g) > -1 ||
+            temp.substring(0, 1).search(/[0-9]/g) > -1
+        ) {
+            temp = '\\' + temp + ' ';
+        }
+    }
+    return temp;
+}
+
+export function generateNodeName(node: Node, currentCount: number, totalCount: number): string {
+    if (node.verilogLabel) return node.verilogLabel;
+    const parentVerilogLabel = node.parent.verilogLabel;
+    let nodeName: string;
+    if (node.label) {
+        nodeName = sanitizeLabel(node.label);
+    } else {
+        nodeName = totalCount > 1 ? 'out_' + currentCount : 'out';
+    }
+    if (parentVerilogLabel.substring(0, 1).search(/\\/g) < 0) {
+        return parentVerilogLabel + '_' + nodeName;
+    } else {
+        return (
+            parentVerilogLabel.substring(0, parentVerilogLabel.length - 1) +
+            '_' +
+            nodeName +
+            ' '
+        );
+    }
+}
