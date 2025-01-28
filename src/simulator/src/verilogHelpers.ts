23019a03729b203daab7392a8007088bbbae3898
Harsh Rao <harshraocodesup@gmail.com>
2025-01-28 08:10:39 +0530

    refactor for ts integration

--- src/simulator/src/verilogHelpers.ts
+++ src/simulator/src/verilogHelpers.ts
@@ -6,45 +6,60 @@ interface Node {
     label?: string;
 }
 
-export function sanitizeLabel(name: string): string {
-    let temp = name;
-    // if there is a space anywhere but the last place
-    // replace spaces by "_"
-    // last space is required for escaped id
-    if (temp.search(/ /g) < temp.length - 1 && temp.search(/ /g) >= 0) {
-        temp = temp.replace(/ Inverse/g, '_inv');
-        temp = temp.replace(/ /g, '_');
+/**
+ * Replaces spaces in the string with underscores, except for the last space.
+ * @param str - The input string.
+ * @returns The string with spaces replaced by underscores.
+ */
+function replaceSpaces(str: string): string {
+    if (str.search(/ /g) < str.length - 1 && str.search(/ /g) >= 0) {
+        return str.replace(/ Inverse/g, '_inv').replace(/ /g, '_');
     }
-    // if first character is not \ already
-    if (temp.substring(0, 1).search(/\\/g) < 0) {
-        // if there are non-alphanum_ character, or first character is num, add \
-        if (
-            temp.search(/[\W]/g) > -1 ||
-            temp.substring(0, 1).search(/[0-9]/g) > -1
-        ) {
-            temp = '\\' + temp + ' ';
-        }
-    }
-    return temp;
+    return str;
 }
 
-export function generateNodeName(node: Node, currentCount: number, totalCount: number): string {
-    if (node.verilogLabel) return node.verilogLabel;
-    const parentVerilogLabel = node.parent.verilogLabel;
-    let nodeName: string;
-    if (node.label) {
-        nodeName = sanitizeLabel(node.label);
-    } else {
-        nodeName = totalCount > 1 ? 'out_' + currentCount : 'out';
+/**
+ * Escapes the string if it starts with a number or contains non-alphanumeric characters.
+ * @param str - The input string.
+ * @returns The escaped string.
+ */
+function escapeString(str: string): string {
+    if (str.substring(0, 1).search(/\\/g) < 0) {
+        if (str.search(/[\W]/g) > -1 || str.substring(0, 1).search(/[0-9]/g) > -1) {
+            return `\\${str} `;
+        }
     }
+    return str;
+}
+
+/**
+ * Sanitizes a label by replacing spaces and escaping special characters.
+ * @param name - The label to sanitize.
+ * @returns The sanitized label.
+ */
+export function sanitizeLabel(name: string): string {
+    let temp = replaceSpaces(name);
+    return escapeString(temp);
+}
+
+/**
+ * Generates a node name based on the node's properties and context.
+ * @param node - The node object.
+ * @param currentCount - The current count of nodes.
+ * @param totalCount - The total count of nodes.
+ * @returns The generated node name.
+ */
+export function generateNodeName(node: Node, currentCount: number, totalCount: number): string {
+    if (node.verilogLabel) {
+        return node.verilogLabel;
+    }
+
+    const parentVerilogLabel = node.parent.verilogLabel;
+    const nodeName = node.label ? sanitizeLabel(node.label) : totalCount > 1 ? `out_${currentCount}` : 'out';
+
     if (parentVerilogLabel.substring(0, 1).search(/\\/g) < 0) {
-        return parentVerilogLabel + '_' + nodeName;
+        return `${parentVerilogLabel}_${nodeName}`;
     } else {
-        return (
-            parentVerilogLabel.substring(0, parentVerilogLabel.length - 1) +
-            '_' +
-            nodeName +
-            ' '
-        );
+        return `${parentVerilogLabel.substring(0, parentVerilogLabel.length - 1)}_${nodeName} `;
     }
 }
