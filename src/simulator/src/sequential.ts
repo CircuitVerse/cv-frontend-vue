d05acecebbd9febe39fb41b665ee1cb283971fe8
Harsh Rao <harshraocodesup@gmail.com>
2025-01-28 15:06:55 +0530

    refactor : ts integration

---
+++ src/simulator/src/sequential.ts
@@ -0,0 +1,11 @@
+import { scheduleUpdate, play, updateCanvasSet } from './engine';
+import { simulationArea } from './simulationArea';
+
+/**
+ * A global function as a helper for simulationArea.changeClockEnable
+ * @param val - The new value for clockEnabled.
+ * @category sequential
+ */
+export function changeClockEnable(val: boolean): void {
+    simulationArea.clockEnabled = val;
+}
