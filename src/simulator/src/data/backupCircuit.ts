831ca94c031b2c91ff54d3c87b3dc8f5e3b359df
Alisha <mahmoodalisha35@gmail.com>
2025-02-04 17:36:40 +0530

    convert backupCircuit code to TypeScript

--- src/simulator/src/data/backupCircuit.ts
+++ src/simulator/src/data/backupCircuit.ts
@@ -1,8 +1,11 @@
 import { projectSavedSet } from './project'
 import { moduleList, updateOrder } from '../metadata'
+import { Scope, Node, BackupData } from '../types/Scope'
+
+declare const globalScope: Scope;
 
 /* eslint-disable no-param-reassign */
-function extract(obj) {
+function extract(obj: Node): object {
     return obj.saveObject()
 }
 
@@ -13,54 +16,42 @@ function extract(obj) {
  * @return {boolean}
  * @category data
  */
-export function checkIfBackup(scope) {
+export function checkIfBackup(scope: Scope): boolean {
     for (let i = 0; i < updateOrder.length; i++) {
-        if (scope[updateOrder[i]].length) return true
+        const property = scope[updateOrder[i]];
+        if (Array.isArray(property) && property.length) {
+            return true;
+        }
     }
-    return false
+    return false;
 }
 
-export function backUp(scope = globalScope) {
+
+export function backUp(scope: Scope = globalScope): BackupData {
+    const data: BackupData = {
+        layout: scope.layout,
+        verilogMetadata: scope.verilogMetadata,
+        allNodes: scope.allNodes.map(extract),
+        testbenchData: scope.testbenchData,
+        id: scope.id,
+        name: scope.name,
+        restrictedCircuitElementsUsed: scope.restrictedCircuitElementsUsed,
+        nodes: scope.nodes.map((node: Node) => scope.allNodes.indexOf(node)),   
+    }
+
+    // Storing details of all module objects
+    for (let i = 0; i < moduleList.length; i++) {
+        const moduleProperty = scope[moduleList[i]];
+        if (Array.isArray(moduleProperty) && moduleProperty.length) {
+            data[moduleList[i]] = moduleProperty.map(extract);
+        }
+    }
+    
     // Disconnection of subcircuits are needed because these are the connections between nodes
     // in current scope and those in the subcircuit's scope
     for (let i = 0; i < scope.SubCircuit.length; i++) {
         scope.SubCircuit[i].removeConnections()
     }
-
-    var data = {}
-
-    // Storing layout
-    data.layout = scope.layout
-
-    // Storing Verilog Properties
-    data.verilogMetadata = scope.verilogMetadata
-
-    // Storing all nodes
-    data.allNodes = scope.allNodes.map(extract)
-
-    // Storing test attached to scope
-    data.testbenchData = scope.testbenchData
-
-    // Storing other details
-    data.id = scope.id
-    data.name = scope.name
-
-    // Storing details of all module objects
-    for (let i = 0; i < moduleList.length; i++) {
-        if (scope[moduleList[i]].length) {
-            data[moduleList[i]] = scope[moduleList[i]].map(extract)
-        }
-    }
-
-    // Adding restricted circuit elements used in the save data
-    data.restrictedCircuitElementsUsed = scope.restrictedCircuitElementsUsed
-
-    // Storing intermediate nodes (nodes in wires)
-    data.nodes = []
-    for (let i = 0; i < scope.nodes.length; i++) {
-        data.nodes.push(scope.allNodes.indexOf(scope.nodes[i]))
-    }
-
     // Restoring the connections
     for (let i = 0; i < scope.SubCircuit.length; i++) {
         scope.SubCircuit[i].makeConnections()
@@ -69,8 +60,8 @@ export function backUp(scope = globalScope) {
     return data
 }
 
-export function scheduleBackup(scope = globalScope) {
-    var backup = JSON.stringify(backUp(scope))
+export function scheduleBackup(scope: Scope = globalScope): string {
+    const backup = JSON.stringify(backUp(scope))
     if (
         scope.backups.length === 0 ||
         scope.backups[scope.backups.length - 1] !== backup
