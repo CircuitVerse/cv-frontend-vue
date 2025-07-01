7ae0b8342fcf0b906905a25dc069b147f1160bf0
Nihal Rajpal <imdev.nihal@gmail.com>
2025-07-01 08:06:03 +0530

    feat(gsoc2025): add shift register

---
+++ v0/src/simulator/src/sequential/ShiftRegister.js
@@ -0,0 +1,222 @@
+/* eslint-disable no-bitwise */
+import CircuitElement from '../circuitElement'
+import Node, { findNode } from '../node'
+import simulationArea from '../simulationArea'
+import { correctWidth, lineTo, moveTo, fillText4 } from '../canvasApi'
+import { colors } from '../themer/themer'
+
+export default class ShiftRegister extends CircuitElement {
+
+    constructor(x, y, scope = globalScope, dir = 'DOWN', bitWidth = 1, noOfStages = 4, isParallelLoad = "Yes") {
+        super(x, y, scope, dir, bitWidth)
+
+        this.message = 'ShiftRegister'
+
+        this.width = 60;
+
+        this.noOfStages = noOfStages || parseInt(prompt('Enter number of stages:'))
+
+
+        this.isParallelLoad = isParallelLoad ?? "Yes";
+
+        const baseHeight = 230;
+        const extraHeight = this.noOfStages * 20;
+        this.height = baseHeight + extraHeight;
+
+        // Set half-width, half-height for bounding box
+        this.setDimensions(this.width / 2, baseHeight / 2)
+
+        this.rectangleObject = false
+
+        this.inp = []
+        this.out = []
+
+        this.firstInput = new Node(30, 0, 0, this, this.bitWidth, 'First Input')
+
+
+        this.reset = new Node(30, -90, 0, this, 1, 'Reset')
+        this.shiftLoad = new Node(30, -70, 0, this, 1, 'Shift/Load')
+        this.clk = new Node(30, -50, 0, this, 1, "Clock");
+
+        let i = 0;
+
+        while (i < this.noOfStages) {
+            if (this.isParallelLoad == "Yes") {
+                const a = new Node(30, 20 * (i + 1), 0, this, this.bitWidth)
+                this.inp.push(a)
+
+                const b = new Node(-30, 20 * (i + 1), 1, this, this.bitWidth)
+                b.value = 1;
+                this.out.push(b)
+            }
+            ++i;
+        }
+        if (this.isParallelLoad == "No") {
+            const b = new Node(-30, 20 * i, 1, this, this.bitWidth)
+            b.value = 1;
+            this.out.push(b)
+        }
+        this.lastClk = 0
+        this.cell = new Array(this.noOfStages)
+    }
+
+    customDraw() {
+        const ctx = simulationArea.context
+        const xx = this.x
+        const yy = this.y
+
+        const width = this.width
+        const baseHeight = 230; // fixed top height
+        const extraHeight = this.noOfStages * 20; // dynamic growth below
+
+        const partitionY = -30
+
+        // Draw main rectangle
+                ctx.strokeStyle = colors['stroke']
+        ctx.fillStyle = colors['fill']
+        ctx.beginPath();
+
+        // top-left
+        moveTo(ctx, -width / 2, -baseHeight / 2, xx, yy, this.direction);
+
+        // top-right
+        lineTo(ctx, width / 2, -baseHeight / 2, xx, yy, this.direction);
+
+        // bottom-right (extends with noOfStages)
+        lineTo(ctx, width / 2, extraHeight + 20, xx, yy, this.direction);
+
+        // bottom-left
+        lineTo(ctx, -width / 2, extraHeight + 20, xx, yy, this.direction);
+
+        ctx.closePath();
+        ctx.fill();
+        ctx.strokeStyle = colors['stroke']
+        ctx.fillStyle = colors['fill']
+        ctx.lineWidth = correctWidth(3)
+        ctx.stroke();
+
+        // Partition line
+        ctx.beginPath()
+        moveTo(ctx, -width / 2, partitionY, xx, yy, this.direction)
+        lineTo(ctx, width / 2, partitionY, xx, yy, this.direction)
+
+        ctx.setLineDash([4, 2])
+        ctx.stroke()
+        ctx.setLineDash([])
+
+        // Section labels
+        ctx.fillStyle = 'black'
+        ctx.textAlign = 'center'
+        fillText4(ctx, '>>>', 0, 0, xx, yy, this.direction, 10)
+
+        // ==== Label control inputs (inside component) ====
+        fillText4(ctx, 'Reset', 16, -90, xx, yy, this.direction, 6)
+        fillText4(ctx, 'S/L', 16, -70, xx, yy, this.direction, 6)
+        fillText4(ctx, 'Clock', 16, -50, xx, yy, this.direction, 6)
+
+        // ==== Label data i/o ====
+        for (let i = 0; i < this.noOfStages; i++) {
+            fillText4(ctx, `In${i}`, 20, 20 * (i + 1), xx, yy, this.direction, 6)
+            fillText4(ctx, `Out${i}`, -20, 20 * (i + 1), xx, yy, this.direction, 6)
+            fillText4(ctx, this.cell[i] == undefined ? 'x' : this.cell[i], 0, 20 * (i + 1), xx, yy, this.direction, 11)
+        }
+
+    }
+
+    isResolvable() {
+        return true;
+    }
+
+    resolve() {
+        const clkValue = this.clk.value;
+        // Rising edge detection
+        if (this.lastClk === 0 && clkValue === 1) {
+            if (this.reset.value === 1) {
+                this.cell.fill(0);
+            }
+            else if (this.shiftLoad.value === 1) {
+                for (let i = 0; i < this.noOfStages; i++) {
+                    this.cell[i] = this.inp[i].value;
+                }
+            }
+            else {
+                this.cell.unshift(this.firstInput.value)
+                this.cell.length = this.noOfStages
+            }
+
+            for (let i = 0; i < this.noOfStages; i++) {
+                this.out[i].value = this.cell[i];
+                simulationArea.simulationQueue.add(this.out[i])
+            }
+        }
+        this.lastClk = clkValue
+    }
+
+    newBitWidth(bitWidth) {
+        for (let i = 0; i < this.noOfStages; i++) {
+            this.inp[i].bitWidth = bitWidth;
+            this.out[i].bitWidth = bitWidth;
+        }
+    }
+
+    customSave() {
+        const data = {
+            nodes: {
+                reset: findNode(this.reset),
+                shiftLoad: findNode(this.shiftLoad),
+                clk: findNode(this.clk),
+                out: this.out.map(findNode),
+            },
+            values: {
+                cell: this.cell,
+            },
+            constructorParamaters: [this.direction, this.bitWidth, this.noOfStages],
+        }
+        return data
+    }
+
+    changeNumberofStages(noOfStages) {
+        if (noOfStages == undefined || noOfStages < 1 || noOfStages > 32) return;
+        if (this.noOfStages == noOfStages) return;
+        var obj = new ShiftRegister(this.x, this.y, this.scope, this.dir, this.bitWidth, noOfStages, this.isParallelLoad)
+        this.delete()
+        simulationArea.lastSelected = obj
+        return obj
+    }
+
+    changeParallelLoad(parallelLoad) {
+        if (parallelLoad !== undefined && this.isParallelLoad !== parallelLoad) {
+            this.isParallelLoad = parallelLoad;
+            var obj = new ShiftRegister(this.x, this.y, this.scope, this.dir, this.bitWidth, this.noOfStages, this.isParallelLoad)
+            this.delete()
+            simulationArea.lastSelected = obj
+            return obj;
+        }
+
+    }
+
+}
+
+
+ShiftRegister.prototype.mutableProperties = {
+    noOfStages: {
+        name: 'Number of Stages: ',
+        type: 'number',
+        max: '32',
+        min: '1',
+        func: 'changeNumberofStages',
+    },
+    isParallelLoad: {
+        name: 'Parallel Load: ',
+        type: 'dropdown',
+        func: 'changeParallelLoad',
+        dropdownArray: ['Yes', 'No']
+    }
+}
+
+ShiftRegister.prototype.helplink =
+    'https://docs.circuitverse.org/#/chapter4/<to be updated>'
+
+ShiftRegister.prototype.tooltipText = "ShiftRegister";
+
+ShiftRegister.prototype.objectType = 'ShiftRegister'
