5f23f6c6f7d8b8ffdb8b344262a659bd5e0e2eff
Nihal Rajpal <imdev.nihal@gmail.com>
2025-07-06 17:28:58 +0530

    feat(gsoc2025): add semicolons & review changes

--- v0/src/simulator/src/modules/Comparator.js
+++ v0/src/simulator/src/modules/Comparator.js
@@ -20,14 +20,14 @@ export default class Comparator extends CircuitElement {
         super(x, y, scope, dir, bitWidth)
 
         this.fixedBitWidth = false;
-        this.setDimensions(20, 20)
+        this.setDimensions(20, 20);
 
-        this.inpA = new Node(-20, -10, 0, this, this.bitWidth, 'A')
-        this.inpB = new Node(-20, +10, 0, this, this.bitWidth, 'B')
+        this.inpA = new Node(-20, -10, 0, this, this.bitWidth, 'A');
+        this.inpB = new Node(-20, +10, 0, this, this.bitWidth, 'B');
 
-        this.less = new Node(20, 10, 1, this, 1, 'less')
-        this.equal = new Node(20, 0, 1, this, 1, 'equal')
-        this.greater = new Node(20, -10, 1, this, 1, 'greater')
+        this.less = new Node(20, 10, 1, this, 1, 'less');
+        this.equal = new Node(20, 0, 1, this, 1, 'equal');
+        this.greater = new Node(20, -10, 1, this, 1, 'greater');
 
     }
 
@@ -45,16 +45,16 @@ export default class Comparator extends CircuitElement {
                 equal: findNode(this.equal),
                 less: findNode(this.less),
             },
-        }
-        return data
+        };
+        return data;
     }
-    
+
     /**
      * Checks whether the component is ready to be resolved
      * @returns {boolean} True if inputs are defined
      */
     isResolvable() {
-        return this.inpA.value != undefined && this.inpB.value != undefined;
+        return this.inpA.value !== undefined && this.inpB.value !== undefined;
     }
 
     /**
@@ -62,18 +62,19 @@ export default class Comparator extends CircuitElement {
      * @param {number} bitWidth - New bit width
      */
     newBitWidth(bitWidth) {
-        this.inpA.bitWidth = bitWidth
-        this.inpB.bitWidth = bitWidth
+        this.inpA.bitWidth = bitWidth;
+        this.inpB.bitWidth = bitWidth;
     }
 
     /**
      * Resolves the comparator outputs based on A and B
      */
     resolve() {
-
         const a = this.inpA.value;
         const b = this.inpB.value;
-
+        this.greater.value = 0;
+        this.equal.value = 0;
+        this.less.value = 0;
         if (a === b) {
             this.greater.value = 0;
             this.equal.value = 1;
@@ -87,24 +88,24 @@ export default class Comparator extends CircuitElement {
             this.equal.value = 0;
             this.less.value = 1;
         }
-        simulationArea.simulationQueue.add(this.greater)
-        simulationArea.simulationQueue.add(this.equal)
-        simulationArea.simulationQueue.add(this.less)
+        simulationArea.simulationQueue.add(this.greater);
+        simulationArea.simulationQueue.add(this.equal);
+        simulationArea.simulationQueue.add(this.less);
     }
 
     /**
      * Custom draw logic for displaying the comparator
      */
     customDraw() {
-        const ctx = simulationArea.context
-        const xx = this.x
-        const yy = this.y
+        const ctx = simulationArea.context;
+        const xx = this.x;
+        const yy = this.y;
 
-        ctx.fillStyle = 'black'
-        ctx.textAlign = 'center'
-        fillText4(ctx, '>', 12, -10, xx, yy, this.direction, 12)
-        fillText4(ctx, '=', 12, 0, xx, yy, this.direction, 12)
-        fillText4(ctx, '<', 12, 10, xx, yy, this.direction, 12)
+        ctx.fillStyle = 'black';
+        ctx.textAlign = 'center';
+        fillText4(ctx, '>', 12, -10, xx, yy, this.direction, 12);
+        fillText4(ctx, '=', 12, 0, xx, yy, this.direction, 12);
+        fillText4(ctx, '<', 12, 10, xx, yy, this.direction, 12);
     }
 
 
@@ -112,7 +113,7 @@ export default class Comparator extends CircuitElement {
 }
 
 Comparator.prototype.tooltipText =
-    'Comparator'
+    'Comparator';
 Comparator.prototype.helplink =
-    'https://docs.circuitverse.org/#/chapter4/<to be updated>'
-Comparator.prototype.objectType = 'Comparator'
+    'https://docs.circuitverse.org/#/chapter4/<to be updated>';
+Comparator.prototype.objectType = 'Comparator';
