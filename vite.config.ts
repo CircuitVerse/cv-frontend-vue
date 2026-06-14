ecbc1ba1a5c219c15e458ed1ee740f1beb4c776d
Nihal <imdev.nihal@gmail.com>
2026-06-14 17:35:05 +0000

    fix: support separate index.html per version using Vite root-based setup
    fix: use vite root for version folder resolution
    
    fix: i18n locales resolution
    
    fix: vue sim version loading bug
    
    fix: change outDir to project root
    
    fix: format vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "url";
+import path from "path";
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from "vite-plugin-vuetify";
 
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
+// Extract the build directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -33,15 +38,13 @@ export default defineConfig(() => {
         "@": fileURLToPath(new URL(`./${version}/src/components`, import.meta.url)),
       },
     },
+    root: fileURLToPath(new URL(`./${version}`, import.meta.url)),
     base: process.env.VITE_BASE || (isDesktop ? "/" : `/simulatorvue/${version}/`),
     build: {
-      outDir: `./dist/simulatorvue/${version}/`,
+      outDir: path.resolve(__dirname, `./dist/simulatorvue/${version}/`),
       assetsDir: "assets",
       chunkSizeWarningLimit: 1600,
       rollupOptions: {
-        input: {
-          main: fileURLToPath(new URL(`./${version}/index.html`, import.meta.url)),
-        },
         output: {
           entryFileNames: `simulator-${version}.js`,
           chunkFileNames: `assets/[name]-[hash].js`,
