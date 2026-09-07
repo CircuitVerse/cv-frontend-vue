f36aa72ed878a34015137dcfbd31c8dc414b506c
Ujjwal Verma <ujjwalverma010305@gmail.com>
2026-09-07 20:28:13 +0530

    fix: remove unused imports from clipboard.ts

--- v1/src/simulator/src/commands/clipboard.ts
+++ v1/src/simulator/src/commands/clipboard.ts
@@ -1,6 +1,4 @@
 import { copy, paste, selectAll } from "../events";
-import undo from "../data/undo";
-import redo from "../data/redo";
 import { deleteSelected } from "../ux";
 
 export { copy, paste, selectAll, deleteSelected };
