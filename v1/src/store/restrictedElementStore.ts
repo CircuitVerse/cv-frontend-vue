412e49112f0f3b814f1254d02b8aecd40c47dd72
Ujjwal Verma <ujjwalverma010305@gmail.com>
2026-09-11 16:09:18 +0530

    style: fix formatting in restrictedElementStore

--- v1/src/store/restrictedElementStore.ts
+++ v1/src/store/restrictedElementStore.ts
@@ -1,9 +1,9 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useRestrictedElementStore = defineStore('restrictedElement', () => {
-  const isHoverVisible = ref(false)
-  const usedElements = ref<string[]>([])
+export const useRestrictedElementStore = defineStore("restrictedElement", () => {
+  const isHoverVisible = ref(false);
+  const usedElements = ref<string[]>([]);
 
-  return { isHoverVisible, usedElements }
-})
+  return { isHoverVisible, usedElements };
+});
