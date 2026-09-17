bf745fa57d604b7f66a7023b6dc6d3403efbeaae
Ujjwal Verma <ujjwalverma010305@gmail.com>
2026-09-17 15:44:08 +0530

    feat(simulator): move keyboard, clipboard and Tauri bindings into composables

--- v1/src/pages/simulator.vue
+++ v1/src/pages/simulator.vue
@@ -12,6 +12,9 @@ import Extra from '@/Extra.vue'
 import { defineComponent, onMounted } from 'vue'
 import { setup as setupSimulator } from '../simulator/src/setup'
 import Helper from '#/components/helpers/Helper.vue'
+import { useSimulatorShortcuts } from '#/composables/useSimulatorShortcuts'
+import { useSimulatorClipboard } from '#/composables/useSimulatorClipboard'
+import { useTauriSimulatorCommands } from '#/composables/useTauriSimulatorCommands'
 
 defineComponent({
     components: {
@@ -20,6 +23,12 @@ defineComponent({
         Extra,
     },
 })
+
+// Global keyboard, clipboard and desktop-menu bindings live for the
+// lifetime of the simulator page.
+useSimulatorShortcuts()
+useSimulatorClipboard()
+useTauriSimulatorCommands()
 
 onMounted(() => {
     setupSimulator()
